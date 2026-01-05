2d1889ee79046dcc88e86f98bbd2fcd7a7f5fad5
Balasaravanan <balan7122001@gmail.com>
2026-01-05 23:05:06 +0530

    05-01-26 Practiced.

--- Rotaion/rotaion.cpp
+++ Rotaion/rotaion.cpp
@@ -12,17 +12,42 @@ int main()
         return -1;
     }
 
-    Mat rotated_mat = getRotationMatrix2D(Point2f(img.size().width / 2, img.size().height / 2), 90, 1);
-    Mat rotated_img;
-    warpAffine(img, rotated_img, rotated_mat, img.size() * 2);
-    Mat transpose_img;
-    transpose(rotated_img, transpose_img);
-
     imshow("Original Image", img);
-    imshow("Rotated Image", rotated_img);
-    imshow("Transpose Image", transpose_img);
+    Point2f center(img.cols / 2.0f, img.rows / 2.0f);
+
+    for (int i = 0; i <= 360; i++)
+    {
+        // Get Rotaion 2 x 3 matrix
+        Mat rotated_mat = getRotationMatrix2D(center, i, 1);
+
+        cout << "rotated_mat: " << endl
+             << rotated_mat << endl;
+
+        // rotated_mat.at<double>(0, 2) += 50;
+        // rotated_mat.at<double>(1, 2) += 50;
+
+        // cout << "new rotated_mat: " << endl
+        //      << rotated_mat << endl;
+
+        Mat rotated_img;
+        warpAffine(img, rotated_img, rotated_mat, img.size() * 2, INTER_CUBIC);
+
+        imshow("Rotated Image", rotated_img);
+        waitKey(0);
+    }
+
+    // Mat rotated_mat = getRotationMatrix2D(Point2f(img.size().width / 2, img.size().height / 2), 90, 1);
+    // Mat rotated_img;
+    // warpAffine(img, rotated_img, rotated_mat, img.size() * 2);
+
+    // Mat transpose_img;
+    // transpose(rotated_img, transpose_img);
+
+    // imshow("Rotated Image", rotated_img);
+    // imshow("Transpose Image", transpose_img);
     waitKey(0);
 
     destroyAllWindows();
     return 0;
 }
+
